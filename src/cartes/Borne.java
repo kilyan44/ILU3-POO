c3da6c71464fd45a62ecb3a98a5407fd07aebf14
kilyan44 <k.delhoume@gmail.com>
2026-09-23 16:18:13 +0200

    first

---
+++ src/cartes/Borne.java
@@ -0,0 +1,13 @@
+package cartes;
+
+public class Borne extends Carte {
+    private Integer km;
+
+    public Borne(Integer km) {
+        this.km = km;
+    }
+
+    public Integer getKm() {
+        return km;
+    }
+}
